a6442519935ab993701769d2f7c82365bd33634a
cedricir <cedricir@student.21-school.ru>
2025-11-30 15:38:04 +0300

    zuerst ändern

--- tasks/tsyplakov_k_vec_neighbours/mpi/src/ops_mpi.cpp
+++ tasks/tsyplakov_k_vec_neighbours/mpi/src/ops_mpi.cpp
@@ -26,44 +26,62 @@ bool TsyplakovKVecNeighboursMPI::PreProcessingImpl() {
 }
 
 bool TsyplakovKVecNeighboursMPI::RunImpl() {
-  int my_rank = 0;
-  int world_size = 0;
+  int rank = 0, size = 0;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-  MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
-  MPI_Comm_size(MPI_COMM_WORLD, &world_size);
-
-  const auto &arr = GetInput();
-  const int n = static_cast<int>(arr.size());
+  const auto &full_arr = GetInput();
+  int n = static_cast<int>(full_arr.size());
 
   if (n < 2) {
     GetOutput() = std::make_tuple(-1, -1);
     return true;
   }
 
+  std::vector<int> sendcounts(size), displs(size);
+
+  int base = n / size;
+  int extra = n % size;
+
+  int offset = 0;
+  for (int i = 0; i < size; i++) {
+    sendcounts[i] = base + (i < extra ? 1 : 0);
+    displs[i] = offset;
+    offset += sendcounts[i];
+  }
+
+  for (int i = 0; i < size - 1; i++) {
+    sendcounts[i]++;
+  }
+
+  int recv_count = sendcounts[rank];
+  std::vector<int> local_arr(recv_count);
+
+  MPI_Scatterv(full_arr.data(), sendcounts.data(), displs.data(), MPI_INT, local_arr.data(), recv_count, MPI_INT, 0,
+               MPI_COMM_WORLD);
+
   int best_local_gap = std::numeric_limits<int>::max();
   int best_local_pos = -1;
 
-  for (int idx = my_rank; idx < n - 1; idx += world_size) {
-    int diff = std::abs(arr[idx] - arr[idx + 1]);
+  int global_offset = displs[rank];
 
+  for (int i = 0; i < recv_count - 1; i++) {
+    int diff = std::abs(local_arr[i] - local_arr[i + 1]);
     if (diff < best_local_gap) {
       best_local_gap = diff;
-      best_local_pos = idx;
+      best_local_pos = global_offset + i;
     }
   }
 
   int best_global_gap = 0;
   MPI_Allreduce(&best_local_gap, &best_global_gap, 1, MPI_INT, MPI_MIN, MPI_COMM_WORLD);
 
-  int candidate_index = std::numeric_limits<int>::max();
-  if (best_local_gap == best_global_gap) {
-    candidate_index = best_local_pos;
-  }
+  int candidate_idx = (best_local_gap == best_global_gap ? best_local_pos : std::numeric_limits<int>::max());
+  int final_idx = 0;
 
-  int final_index = 0;
-  MPI_Allreduce(&candidate_index, &final_index, 1, MPI_INT, MPI_MIN, MPI_COMM_WORLD);
+  MPI_Allreduce(&candidate_idx, &final_idx, 1, MPI_INT, MPI_MIN, MPI_COMM_WORLD);
 
-  GetOutput() = std::make_tuple(final_index, final_index + 1);
+  GetOutput() = std::make_tuple(final_idx, final_idx + 1);
   return true;
 }
 
